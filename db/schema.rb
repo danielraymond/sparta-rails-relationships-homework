c5aea7ed6763354380c69be8f06bd71612a1099c
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-22 22:29:34 +0000

    added controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122221517) do
+ActiveRecord::Schema.define(version: 20171122222026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20171122221517) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "genre_id"
   end
 
 end
